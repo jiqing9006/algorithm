c16e0c6e9ac359a562c8c36105ecaeedbae942c7
jq_china <jiqing9006@qq.com>
2020-01-17 00:18:34 +0800

    提交

--- test.php
+++ test.php
@@ -1,41 +1,32 @@
 <?php
 /**
- * 5个人偷了一堆苹果
- * 第一个人分成5份，多了一个。拿走其中一份。
- * 第二个人分成5份，多了一个。拿走其中一份。
- * ...
- * 第二天，又分剩下的，又分成5份，多了一个。
- * 问共有多少个苹果？
+ * 跟猴子问题比较相似，最后一个是大王。约瑟夫是最后两个是存活。
+ * 约瑟夫问题是个有名的问题：
+ * N个人围成一圈，从第一个开始报数，第M个将被杀掉，最后剩下一个，其余人都将被杀掉。
+ * 例如N=6，M=5，被杀掉的顺序是：5，4，6，2，3，1。
  */
-function getAppleCount()
+function getLastTwo()
 {
-    // 一个一个加
-    for ($s = 5;; $s++) {
-        if ($s % 5 == 1) {
-            // 算出第1次分苹果后剩下的数量
-            $left_1 = $s - round($s / 5) - 1; // 四舍五入
-            if ($left_1 % 5 == 1) {
-                // 算出第2次分苹果后剩下的数量
-                $left_2 = $left_1 - round($left_1 / 5) - 1;
-                if ($left_2 % 5 == 1) {
-                    // 算出第3次分苹果后剩下的数量
-                    $left_3 = $left_2 - round($left_2 / 5) - 1;
-                    if ($left_3 % 5 == 1) {
-                        // 算出第4次分苹果后剩下的数量
-                        $left_4 = $left_3 - round($left_3 / 5) - 1;
-                        if ($left_4 % 5 == 1) {
-                            // 算出第5次分苹果后剩下的数量
-                            $left_5 = $left_4 - round($left_4 / 5) - 1;
-                            if ($left_5 % 5 == 1) {
-                                echo $s;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
+    $people = 41;
+    $killNum = 3;
+
+    // 初始化数据
+    $people_arr = range(1, $people);
+
+    for ($i = 1; $i <= $killNum * ($people - 2); $i++) {
+        $speak_num = array_shift($people_arr);
+        if ($i % $killNum == 0) {
+            echo "死了" . $speak_num . "<br/>";
+        } else {
+            // 放到后面
+            array_push($people_arr, $speak_num);
         }
+    }
+
+    // 输出活着的人
+    foreach ($people_arr as $alive) {
+        echo "我" . $alive . "还活着！<br/>";
     }
 }
 
-echo getAppleCount(); // 15621 这是第一个符合条件的值
+getLastTwo();
